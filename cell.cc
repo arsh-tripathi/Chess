8be937c9e6994abeb4f6477603ac7fd09714506b
Arsh Tripathi <arsht2004@gmail.com>
2023-11-26 20:38:04 -0500

    Completed cell

--- cell.cc
+++ cell.cc
@@ -1,49 +1,83 @@
 #include "cell.h"
+#include "coord.h"
+#include "enums.h"
+#include <memory>
+#include <utility>
 
-Cell::Cell(Coord coordinate, Piece* p): coordinate{coordinate}, p{p} {}
-Cell::~Cell() {}
-    
+using namespace std;
+
+Cell::Cell(Coord coordinate) : coordinate{coordinate}, p{nullptr}
+{
+}
+Cell::Cell(Coord coordinate, Piece p) : coordinate{coordinate}, p{make_unique<Piece>(p)}
+{
+}
+Cell::~Cell()
+{
+}
+
 // I don't think we need copy/move ctor or assignment operator
 
 // switches the pieces
-void Cell::move(Cell& c) {
+void Cell::move(Cell &dest)
+{
     // when is move is confirmed valid by board, calls cell->move()
 
-    // this moves piece from (this) position to Cell&c
+    // this moves piece from (this) position to Cell& dest
+    notify(*this, dest);
 
     // check if capture (if capture we must delete piece at that point)
     // no need to check for allied piece since this is already checked in board.move()
-
+    notifyDisplayObservers(dest); // updates display
 }
 
 // tells display observers that piece has been moved
-void Cell::notifyDisplayObservers() {
-
+void Cell::notifyDisplayObservers(Cell &dest)
+{
+    for (size_t i = 0; i < observers.size(); ++i)
+    {
+        if (observers[i]->subType() == SubscriptionType::Display)
+        {
+            observers[i]->notify(*this, dest);
+        }
+    }
 }
 
-// not sure what this is going to be used for
-void Cell::notifyCellObservers() {
-
+// called by move
+void Cell::notify(Cell &c, Cell &dest)
+{
+    // actually performs the move swapping and shit(commit this shit)
+    unique_ptr<Piece> nptr = nullptr;
+    swap(dest.p, c.p);
+    c.detachAllCellObservers();
+    swap(c.p, nptr);
+    //
+    // #########################################################
+    // #########################################################
+    //
+    // you can pass nptr to undo to store it and it will not deallocate it
+    // happy happy
 }
 
-// calls move
-void Cell::notify(Cell& c, Cell& dest) {
-    
-}
-
-SubscriptionType Cell::subType() {
+SubscriptionType Cell::subType()
+{
     return SubscriptionType::GameSquares;
 }
 
 // when piece is moved to new cell, we attach all cell
-void Cell::attach(Observer* o) {
+void Cell::attach(Observer *o)
+{
     observers.emplace_back(o);
 }
 
 // when piece is moved away from cell, we detach all cell observers
-void Cell::detachAllCellObservers() {
-    for (auto it = observers.begin(); it != observers.end();) {
-        if ((*it)->subType() == SubscriptionType::GameSquares) observers.erase(it);
-        else ++it;
+void Cell::detachAllCellObservers()
+{
+    for (auto it = observers.begin(); it != observers.end();)
+    {
+        if ((*it)->subType() == SubscriptionType::GameSquares)
+            observers.erase(it);
+        else
+            ++it;
     }
 }
--- cell.h
+++ cell.h
@@ -1,59 +1,57 @@
 #ifndef __CELL_H__
 #define __CELL_H__
 
-#include "observer.h"
 #include "coord.h"
+#include "observer.h"
 #include "piece.h"
 
 #include <vector>
 
-using namespace std;
-
-class Cell: public Observer {
+class Cell : public Observer
+{
 
     Coord coordinate;
-    
-    // definetly going to be shared pointers
-    vector<Observer*> observers; // textDisplay, graphicsDisplay and all possibleMoves Cells
-    
-    Piece* p;
 
-    public:
-    Cell(Coord coordinate, Piece* p);
+    // definetly going to be shared pointers
+    std::vector<std::unique_ptr<Observer>> observers; // textDisplay, graphicsDisplay and all possibleMoves Cells
+
+    std::unique_ptr<Piece> p;
+
+  public:
+    Cell(Coord coordinate);
+    Cell(Coord coordinate, Piece p);
     ~Cell();
-    
+
     // I don't think we need copy/move ctor or assignment operator
 
     // switches the pieces
-    void move(Cell& c); 
-    
-    // tells display observers that piece has been moved
-    void notifyDisplayObservers();
+    void move(Cell &dest);
 
-    // not sure what this is going to be used for
-    void notifyCellObservers();
-    
+    // tells display observers that piece has been moved
+    void notifyDisplayObservers(Cell &dest);
+
     // calls move
-    void notify(Cell& c, Cell& dest) override;
-    
+    void notify(Cell &c, Cell &dest) override;
+
     SubscriptionType subType() override;
-    
+
     // when piece is moved to new cell, we attach all cell
-    void attach(Observer*);
+    void attach(std::unique_ptr<Observer> o);
 
     // when piece is moved away from cell, we detach all cell observers
     void detachAllCellObservers();
 
     // getters
 
-    Coord getCoordinate() {
+    Coord getCoordinate()
+    {
         return coordinate;
     }
 
-    Piece* getPiece() {
+    Piece *getPiece()
+    {
         return p;
     }
-
 };
 
 #endif
