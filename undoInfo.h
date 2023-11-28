b6ba1f0e948e75e80b6966a768e3bd600909bf2c
ozpan <ozpan@uwaterloo.ca>
2023-11-28 17:51:23 -0500

    undoInfo struct

---
+++ undoInfo.h
@@ -0,0 +1,19 @@
+#ifndef __UNDOINFO_H__
+#define __UNDOINFO_H__
+
+#include "coord.h"
+#include "enums.h"
+#include "piece.h"
+
+// move struct
+struct UndoInfo {
+    Coord start; // needs default constructor
+    Coord end; // needs default constructor
+    State status;
+    Piece* originalEndPiece;
+    
+    UndoInfo(): start{Coord{-1,-1}}, end{Coord{-1,-1}}, status{State::Normal}, originalEndPiece{nullptr} {}
+    UndoInfo(Coord start, Coord end, State status, Piece* og): start{start}, end{end}, status{status}, originalEndPiece{og} {}
+};
+
+#endif
